64c0b05dc8f9e7cd65b7e99656cee8c623dd0e33
Kyle Affolder <kyleaffolder@gmail.com>
2026-01-09 11:30:02 -0500

    Update: Prevent brew shellenv
    Prevent it from running on every interactive shell (improves terminal instance startup)

--- xdg/zsh/zshrc.d/10-homebrew.zsh
+++ xdg/zsh/zshrc.d/10-homebrew.zsh
@@ -1,9 +1,17 @@
+# Homebrew: initialize PATH/env once, even if brew isn't on PATH yet.
+brew_bin=""
+
 if (( $+commands[brew] )); then
-  eval "$(brew shellenv)"
-else
-  if [[ -x /opt/homebrew/bin/brew ]]; then
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-  elif [[ -x /usr/local/bin/brew ]]; then
-    eval "$(/usr/local/bin/brew shellenv)"
+  brew_bin="$(command -v brew)"
+elif [[ -x /opt/homebrew/bin/brew ]]; then
+  brew_bin="/opt/homebrew/bin/brew"
+elif [[ -x /usr/local/bin/brew ]]; then
+  brew_bin="/usr/local/bin/brew"
+fi
+
+if [[ -n "$brew_bin" ]]; then
+  # Only run shellenv if Homebrew env vars aren't already present.
+  if [[ -z "${HOMEBREW_PREFIX-}" || -z "${HOMEBREW_CELLAR-}" || -z "${HOMEBREW_REPOSITORY-}" ]]; then
+    eval "$("$brew_bin" shellenv)"
   fi
 fi
