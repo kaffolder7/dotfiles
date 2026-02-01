ef1600c1bb56a67faeeba47b6f5c9189757d3cde
Kyle Affolder <kyleaffolder@gmail.com>
2026-02-01 18:47:35 -0500

    refactor: update home.nix structure
    Reformat the module to improve readability by adjusting the parameter list to a multi-line layout and streamlining the logic for setting the home directory path. This change enhances maintainability and aligns the code with common Nix conventions for better clarity and structure.

--- nix/home.nix
+++ nix/home.nix
@@ -1,16 +1,19 @@
 # Run (if `home-manager` is on `PATH`):
 #  - `home-manager switch --flake .#macmini`, or...
 #  - `home-manager switch --flake .#mbp`
-{ config, pkgs, lib, username, ghosttyOneDark, ... }:
+{
+  pkgs,
+  lib,
+  username,
+  ghosttyOneDark,
+  ...
+}:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = username;
-  home.homeDirectory =
-    if pkgs.stdenv.isDarwin
-    then "/Users/${username}"
-    else "/home/${username}";
+  home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -19,7 +22,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "25.11";  # Please read the comment before changing.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -364,7 +367,7 @@
         # ---- Prompt (P10k) ----
         source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
         # [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
-        [[ -r "${ZDOTDIR:-$HOME}/.p10k.zsh" ]] && source "${ZDOTDIR:-$HOME}/.p10k.zsh"
+        [[ -r "${"ZDOTDIR:-$HOME"}/.p10k.zsh" ]] && source "${"ZDOTDIR:-$HOME"}/.p10k.zsh"
 
         # ---- Run fastfetch once, after prompt is ready ----
         autoload -Uz add-zsh-hook
